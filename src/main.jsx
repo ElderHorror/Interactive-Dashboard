6c1e93ffbd6a9dc6e5372dd0adff97cbef7b0b6c
Adedeji Adebayo <107815490+ElderHorror@users.noreply.github.com>
2025-04-08 21:17:50 +0100

    Fix vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React, { StrictMode } from 'react'
 import ReactDOM,{ createRoot } from 'react-dom/client'
+import { Analytics } from "@vercel/analytics/react"
 import './index.css'
 import App from './App.jsx'
 
